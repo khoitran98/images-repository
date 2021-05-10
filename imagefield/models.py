926138f875253796d7c0ed2c397d6f30b9983e48
Khoi Tran <trandangkhoi1998@gmail.com>
2021-05-10 10:15:32 +0700

    basic image upload

--- imagefield/models.py
+++ imagefield/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ImagefieldModel(models.Model): 
+    title = models.CharField(max_length = 200) 
+    img = models.ImageField(upload_to = "images/")
+
+    class Meta:
+        db_table = "imageupload"
--- imagefield/views.py
+++ imagefield/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import ImagefieldForm
+from .models import ImagefieldModel 
 # Create your views here.
+  
+# Create your views here. 
+def home_view(request): 
+    context = {}
+    if request.method == "POST": 
+        form = ImagefieldForm(request.POST, request.FILES) 
+        if form.is_valid(): 
+            name = form.cleaned_data.get("name") 
+            img = form.cleaned_data.get("image_field") 
+            obj = ImagefieldModel.objects.create( 
+                                 title = name,  
+                                 img = img 
+                                 ) 
+            obj.save() 
+            print(obj)
+            return redirect('success') 
+    else: 
+        form = ImagefieldForm()
+        context['form'] = form
+        return render( request, "fileupload.html", context) 
+
+def success(request): 
+    return HttpResponse('successfully uploaded') 
